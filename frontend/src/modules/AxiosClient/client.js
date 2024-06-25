d9af4c01ba4ef9cc9fc374209de75a9d06d37bcf
Catalin Radoi <radoi.catalin88@gmail.com>
2024-06-25 20:26:29 +0200

    up client

--- frontend/src/modules/AxiosClient/client.js
+++ frontend/src/modules/AxiosClient/client.js
@@ -2,39 +2,42 @@ import axios from "axios";
 
 class AxiosClient {
     static baseUrl = `${process.env.REACT_APP_SERVER_BASE_URL}`;
-    static token = JSON.parse(localStorage.getItem('authorized_user'))
+    static token = JSON.parse(localStorage.getItem('authorized_user'));
 
     constructor() {
+        console.log("Token:", AxiosClient.token); // Log del token
         this.axiosInstance = axios.create({
             baseURL: AxiosClient.baseUrl,
             maxContentLength: Infinity,
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': AxiosClient.token
+                'Authorization': AxiosClient.token ? `Bearer ${AxiosClient.token}` : ''
             }
-        })
+        });
     }
 
     setHeaders(headers) {
         this.axiosInstance.defaults.headers.common = {
             ...this.axiosInstance.defaults.headers.common,
             ...headers
-        }
+        };
     }
 
     async get(url, config) {
-        console.log(AxiosClient.token);
-        const res = await this.axiosInstance.get(url, config)
-        return res.data
+        console.log("GET Token:", this.axiosInstance.defaults.headers.common['Authorization']); // Log del token
+        const res = await this.axiosInstance.get(url, config);
+        return res.data;
     }
 
     async post(url, payload) {
-
+        console.log("POST Token:", this.axiosInstance.defaults.headers.common['Authorization']); // Log del token
         const res = await this.axiosInstance.post(url, payload);
         return res.data;
     }
+
     async postFormData(url, formData) {
+        console.log("POST FormData Token:", this.axiosInstance.defaults.headers.common['Authorization']); // Log del token
         const res = await this.axiosInstance.post(url, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
@@ -44,25 +47,26 @@ class AxiosClient {
         return res.data;
     }
 
-
     async update(url, payload, config) {
-        return await this.axiosInstance.patch(url, payload, config)
+        console.log("PATCH Token:", this.axiosInstance.defaults.headers.common['Authorization']); // Log del token
+        return await this.axiosInstance.patch(url, payload, config);
     }
 
     async updateFormData(url, formData) {
-        console.log(formData);
+        console.log("PATCH FormData Token:", this.axiosInstance.defaults.headers.common['Authorization']); // Log del token
         const res = await this.axiosInstance.patch(url, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
                 ...this.axiosInstance.defaults.headers.common
             }
         });
-
+        return res.data;
     }
 
     async delete(url, config) {
-        return await this.axiosInstance.delete(url, config)
+        console.log("DELETE Token:", this.axiosInstance.defaults.headers.common['Authorization']); // Log del token
+        return await this.axiosInstance.delete(url, config);
     }
 }
 
-export default AxiosClient
+export default AxiosClient;
